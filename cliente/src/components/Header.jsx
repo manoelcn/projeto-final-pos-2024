53c67179de57b6a155a53c79f6006d9e45f7c2b6
Manoel Cândido <candidomanoel78@gmail.com>
2024-12-20 08:07:03 -0300

    add links no header

--- cliente/src/components/Header.jsx
+++ cliente/src/components/Header.jsx
@@ -5,6 +5,8 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
             <div className="container-fluid">
                 <a className="navbar-brand text-white" href="/">StockSimplify</a>
+                <a className="navbar-brand text-white" href="/brands">Marcas</a>
+                <a className="navbar-brand text-white" href="/categories">Categorias</a>
             </div>
         </nav>
     );
