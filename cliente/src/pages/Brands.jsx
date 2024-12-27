4685e36f8e734a4f96f80635c9518e3489dadce1
manoel <candidomanoel78@gmail.com>
2024-12-26 22:21:38 -0300

    estruturando html das paginas

--- cliente/src/pages/Brands.jsx
+++ cliente/src/pages/Brands.jsx
@@ -26,31 +26,125 @@ const Brands = () => {
     return (
         <div class="container-fluid">
             <div class="row">
+                <div class="col-md-6">
+                    <form role="form">
+                        <div class="form-group">
+
+                            <label for="exampleInputEmail1">
+                                Email address
+                            </label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" />
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-6">
+                    <div class="btn-group" role="group">
+
+                        <button class="btn btn-secondary" type="button">
+                            Left
+                        </button>
+                        <button class="btn btn-secondary" type="button">
+                            Center
+                        </button>
+                        <button class="btn btn-secondary" type="button">
+                            Right
+                        </button>
+                        <button class="btn btn-secondary" type="button">
+                            Justify
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
                 <div class="col-md-12">
-                    <h3 class="text-center">
-                        Lista de marcas
-                    </h3>
-                    <table className="table table-hover table-bordered">
+                    <table class="table table-sm table-hover table-bordered">
                         <thead>
                             <tr>
-                                <th>Id</th>
-                                <th>Nome</th>
-                                <th>Descrição</th>
-                                <th>Ações</th>
+                                <th>
+                                    #
+                                </th>
+                                <th>
+                                    Product
+                                </th>
+                                <th>
+                                    Payment Taken
+                                </th>
+                                <th>
+                                    Status
+                                </th>
                             </tr>
                         </thead>
                         <tbody>
-                            {brands.map((brand) => (
-                                <tr key={brand.id}>
-                                    <td>{brand.id}</td>
-                                    <td>{brand.name}</td>
-                                    <td>{brand.description || "N/A"}</td>
-                                    <td>
-                                        <button className="btn btn-primary">Editar</button>
-                                        <button className="btn btn-danger">Excluir</button>
-                                    </td>
-                                </tr>
-                            ))}
+                            <tr>
+                                <td>
+                                    1
+                                </td>
+                                <td>
+                                    TB - Monthly
+                                </td>
+                                <td>
+                                    01/04/2012
+                                </td>
+                                <td>
+                                    Default
+                                </td>
+                            </tr>
+                            <tr class="table-active">
+                                <td>
+                                    1
+                                </td>
+                                <td>
+                                    TB - Monthly
+                                </td>
+                                <td>
+                                    01/04/2012
+                                </td>
+                                <td>
+                                    Approved
+                                </td>
+                            </tr>
+                            <tr class="table-success">
+                                <td>
+                                    2
+                                </td>
+                                <td>
+                                    TB - Monthly
+                                </td>
+                                <td>
+                                    02/04/2012
+                                </td>
+                                <td>
+                                    Declined
+                                </td>
+                            </tr>
+                            <tr class="table-warning">
+                                <td>
+                                    3
+                                </td>
+                                <td>
+                                    TB - Monthly
+                                </td>
+                                <td>
+                                    03/04/2012
+                                </td>
+                                <td>
+                                    Pending
+                                </td>
+                            </tr>
+                            <tr class="table-danger">
+                                <td>
+                                    4
+                                </td>
+                                <td>
+                                    TB - Monthly
+                                </td>
+                                <td>
+                                    04/04/2012
+                                </td>
+                                <td>
+                                    Call in to confirm
+                                </td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
--- cliente/src/pages/Suppliers.jsx
+++ cliente/src/pages/Suppliers.jsx
@@ -5,7 +5,7 @@ const Suppliers = () => {
     const [suppliers, setSuppliers] = useState([]);
     const [error, setError] = useState(null);
 
-    useEffect( () => {
+    useEffect(() => {
         suppliersService
             .listSuppliers()
             .then((data) => {
@@ -21,13 +21,131 @@ const Suppliers = () => {
     }
 
     return (
-        <div>
-            <h1>Lista de fornecedores</h1>
-            <ul>
-                {suppliers.map((supplier) => (
-                    <li key={supplier.id}>{supplier.name}</li>
-                ))}
-            </ul>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6">
+                    <form role="form">
+                        <div class="form-group">
+
+                            <label for="exampleInputEmail1">
+                                Email address
+                            </label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" />
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-6">
+                    <div class="btn-group" role="group">
+
+                        <button class="btn btn-secondary" type="button">
+                            Left
+                        </button>
+                        <button class="btn btn-secondary" type="button">
+                            Center
+                        </button>
+                        <button class="btn btn-secondary" type="button">
+                            Right
+                        </button>
+                        <button class="btn btn-secondary" type="button">
+                            Justify
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <table class="table table-sm table-hover table-bordered">
+                        <thead>
+                            <tr>
+                                <th>
+                                    #
+                                </th>
+                                <th>
+                                    Product
+                                </th>
+                                <th>
+                                    Payment Taken
+                                </th>
+                                <th>
+                                    Status
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    1
+                                </td>
+                                <td>
+                                    TB - Monthly
+                                </td>
+                                <td>
+                                    01/04/2012
+                                </td>
+                                <td>
+                                    Default
+                                </td>
+                            </tr>
+                            <tr class="table-active">
+                                <td>
+                                    1
+                                </td>
+                                <td>
+                                    TB - Monthly
+                                </td>
+                                <td>
+                                    01/04/2012
+                                </td>
+                                <td>
+                                    Approved
+                                </td>
+                            </tr>
+                            <tr class="table-success">
+                                <td>
+                                    2
+                                </td>
+                                <td>
+                                    TB - Monthly
+                                </td>
+                                <td>
+                                    02/04/2012
+                                </td>
+                                <td>
+                                    Declined
+                                </td>
+                            </tr>
+                            <tr class="table-warning">
+                                <td>
+                                    3
+                                </td>
+                                <td>
+                                    TB - Monthly
+                                </td>
+                                <td>
+                                    03/04/2012
+                                </td>
+                                <td>
+                                    Pending
+                                </td>
+                            </tr>
+                            <tr class="table-danger">
+                                <td>
+                                    4
+                                </td>
+                                <td>
+                                    TB - Monthly
+                                </td>
+                                <td>
+                                    04/04/2012
+                                </td>
+                                <td>
+                                    Call in to confirm
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
