1ddf6052e3f815570024f616b11dfb3266000f2c
manoel <candidomanoel78@gmail.com>
2025-01-02 16:25:19 -0300

    mudança na cor da navbar e instalando dependencias

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import Brands from './pages/Brands';
