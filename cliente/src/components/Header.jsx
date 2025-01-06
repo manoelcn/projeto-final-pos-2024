64ed2ba1da08bd1168ada8e7060c7e139134848f
manoel <candidomanoel78@gmail.com>
2025-01-06 15:33:14 -0300

    ajustes temporarios na navbar

--- cliente/src/components/Header.jsx
+++ cliente/src/components/Header.jsx
@@ -11,7 +11,12 @@ function Header() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="/brands">marcas</Nav.Link>
+                        <Nav.Link href="/categories">categorias</Nav.Link>
+                        <Nav.Link href="/inflows">entradas</Nav.Link>
+                        <Nav.Link href="/outflows">saidas</Nav.Link>
+                        <Nav.Link href="/products">produtos</Nav.Link>
+                        <Nav.Link href="/suppliers">fornecedores</Nav.Link>
                         <Nav.Link href="#link">Link</Nav.Link>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
