963f26a90d759b7136d6d3dfb36b0ddb48215209
manoel <candidomanoel78@gmail.com>
2025-01-07 15:29:24 -0300

    fazendo delete de categories!

--- cliente/src/pages/categories/Categories.jsx
+++ cliente/src/pages/categories/Categories.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Table from 'react-bootstrap/Table';
 import categoriesService from '../../services/categoriesService';
+import Modal from "react-bootstrap/Modal"; // Modal do Bootstrap
+import Button from "react-bootstrap/Button";
 
 const Categories = () => {
     const [categories, setCategories] = useState([]);
     const [error, setError] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+    const [categoryDelete, setCategoryDelete] = useState(null);
 
     useEffect(() => {
         categoriesService
@@ -16,6 +20,30 @@ const Categories = () => {
                 setError(error.message);
             });
     }, []);
+
+    const handleShowModal = (category) => {
+        setCategoryDelete(category);
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setCategoryDelete(null);
+        setShowModal(false);
+    };
+
+    const handleDeleteCategory = () => {
+        if (!categoryDelete) return;
+        categoriesService
+            .deleteCategory(categoryDelete.id)
+            .then(() => {
+                setCategories(categories.filter((b) => b.id !== categoryDelete.id));
+                handleCloseModal();
+            })
+            .catch(() => {
+                setError("Erro ao excluir categoria.");
+                handleCloseModal();
+            });
+    }
 
     if (error) {
         return <p>Erro ao carregar as categorias: {error}</p>
@@ -68,11 +96,14 @@ const Categories = () => {
                                             <a href={`/categories/${category.id}`} class="btn btn-info btn-sm">
                                                 <i class="bi bi-eye">Detalhar</i>
                                             </a>
-                                            <a  href={`/categories/${category.id}/edit`} class="btn btn-warning btn-sm">
+                                            <a href={`/categories/${category.id}/edit`} class="btn btn-warning btn-sm">
                                                 <i class="bi bi-pencil">Editar</i>
                                             </a>
-                                            <a href="#" class="btn btn-danger btn-sm">
-                                                <i class="bi bi-trash"></i>
+                                            <a href="#" onClick={(e) => {
+                                                e.preventDefault();
+                                                handleShowModal(category);
+                                            }} class="btn btn-danger btn-sm">
+                                                <i class="bi bi-trash">Excluir</i>
                                             </a>
                                         </td>
                                     </tr>
@@ -88,6 +119,23 @@ const Categories = () => {
                     </Table>
                 </div>
             </div>
+            <Modal show={showModal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirmação de Exclusão</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Tem certeza que deseja excluir a categoria{" "}
+                    <strong>{categoryDelete?.name}</strong>?
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseModal}>
+                        Cancelar
+                    </Button>
+                    <Button variant="danger" onClick={handleDeleteCategory}>
+                        Excluir
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 };
