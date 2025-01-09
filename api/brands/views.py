08efa0aee0d7459af89b8c55c64d3e42bc418f98
manoel <candidomanoel78@gmail.com>
2025-01-09 20:56:07 -0300

    tratando erro

--- api/brands/views.py
+++ api/brands/views.py
@@ -1,5 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from . import models, serializers
+from django.db.models import ProtectedError
+from rest_framework.response import Response
 
 
 class BrandCreateListAPIView(generics.ListCreateAPIView):
@@ -10,3 +12,19 @@ class BrandCreateListAPIView(generics.ListCreateAPIView):
 class BrandRetriveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Brand.objects.all()
     serializer_class = serializers.BrandSerializer
+
+    def delete(self, request, *args, **kwargs):
+            instance = self.get_object()
+            try:
+                self.perform_destroy(instance)
+                return Response({"message": "Marca deletada com sucesso!"}, status=status.HTTP_204_NO_CONTENT)
+            except ProtectedError as e:
+                return Response(
+                    {
+                        "error": "Esta marca não pode ser excluída."
+                    },
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+    def perform_destroy(self, instance):
+            instance.delete()
