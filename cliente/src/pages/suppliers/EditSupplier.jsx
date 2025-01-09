fbdee9d72b8218518220a9b9356c6cda1d27083d
manoel <candidomanoel78@gmail.com>
2025-01-08 21:15:25 -0300

    padronizando pagina de editar fornecedor

--- cliente/src/pages/suppliers/EditSupplier.jsx
+++ cliente/src/pages/suppliers/EditSupplier.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import { useParams, useNavigate } from "react-router-dom";
 import suppliersService from "../../services/suppliersService";
 
@@ -37,7 +39,7 @@ const EditSupplier = () => {
                 setTimeout(() => {
                     setSuccessMessage(null);
                     navigate("/suppliers");
-                }, 1000);
+                });
             })
             .catch(() => {
                 setError("Erro ao atualizar o fornecedor.");
@@ -48,32 +50,36 @@ const EditSupplier = () => {
     if (error) return <p style={{ color: "red" }}>{error}</p>;
 
     return (
-        <div>
-            <h1>Atualizar Fornecedor</h1>
-            {successMessage && <p style={{ color: "green" }}>{successMessage}</p>}
-            {error && <p style={{ color: "red" }}>{error}</p>}
+        <div class="container-fluid mt-4 px-5">
+            <div className="row d-flex justify-content-center align-items-center">
+                <div class="col-md-6">
+                    <div class="col-md-12">
+                        <h3 class="text">
+                            Atualizar Fornecedor
+                        </h3>
+                    </div>
+                    <Card>
+                        {supplier ? (
+                            <Card.Body>
+                                <form className="form" onSubmit={handleUpdateSupplier}>
+                                    <label><strong>Nome</strong></label>
+                                    <input className="form-control" type="text" name="name" value={supplier.name} onChange={handleInputChange} required /><br />
+                                    <label><strong>Descrição</strong></label>
+                                    <textarea className="form-control rows" name="description" value={supplier.description} onChange={handleInputChange} /><br />
 
-            <form onSubmit={handleUpdateSupplier}>
-                <div>
-                    <label>Nome</label>
-                    <input
-                        type="text"
-                        name="name"
-                        value={supplier.name}
-                        onChange={handleInputChange}
-                        required
-                    />
+                                    <Button type="submit">Atualizar</Button>
+                                </form>
+                            </Card.Body>
+                        ) : (
+                            <CardText>
+                                Nenhuma informação encontrada!
+                            </CardText>
+                        )}
+                    </Card>
+                    <br />
+                    <Button href="/suppliers" variant="secondary">Cancelar e Voltar</Button>
                 </div>
-                <div>
-                    <label>Descrição</label>
-                    <textarea
-                        name="description"
-                        value={supplier.description}
-                        onChange={handleInputChange}
-                    ></textarea>
-                </div>
-                <button type="submit">Atualizar</button>
-            </form>
+            </div>
         </div>
     )
 };
